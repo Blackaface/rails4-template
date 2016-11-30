75567e2dd6c84a83bb06ef0be171483de3a8b3a9
marsz <marsz330@gmail.com>
2016-11-30 17:33:08 +0800

    rspec with expect and change

---
+++ spec/models/hello_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe 'Hello' do
+  describe 'normal expect' do
+
+    it 'to eq' do
+      expect(1+1).to eq(2)
+    end
+
+    it 'not to eq' do
+      expect(1+2).not_to eq(2)
+    end
+
+  end
+
+  describe 'string matcher' do
+    it 'include' do
+      expect('abcdef').to include('bcd')
+    end
+  end
+
+  describe 'be' do
+    it 'be >' do
+      expect(1).to be > 0
+    end
+
+    it 'be xxxx? method' do
+      u = User.new
+      expect(u.new_record?).to eq(true)
+      expect(u).to be_new_record
+    end
+  end
+
+  describe 'expect change...to' do
+    it 'normal' do
+      c = Category.new(name: 'haha')
+      expect {
+        c.save
+      }.to change { Category.count }
+    end
+
+    it '.by' do
+      c = Category.new(name: 'haha')
+      expect {
+        c.save
+      }.to change { Category.count }.by(1)
+    end
+
+    it '.to' do
+      c = Category.new(name: 'haha')
+      expect {
+        c.save
+      }.to change { Category.count }.to(1)
+    end
+
+    it '.from ... .to' do
+      c = Category.new(name: 'haha')
+      expect {
+        c.save
+      }.to change { Category.count }.from(0).to(1)
+    end
+  end
+end
