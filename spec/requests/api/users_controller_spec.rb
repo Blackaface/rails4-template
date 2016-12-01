201755e0953d970b5d415b1a8fdda7381e40ceea
marsz <marsz330@gmail.com>
2016-12-01 16:21:58 +0800

    testing json body

--- spec/requests/api/users_controller_spec.rb
+++ spec/requests/api/users_controller_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :request do
 
+  let!(:user) { User.create!(email: 'xxx@example.com', password: '12341234') }
+
   it '#index' do
     get 'http://api.localhost/users.json',
       params: { abc: 1 },
       headers: { 'HTTP_X_TOKEN' => '123456' }
     expect(response.status).to eq(200)
+    expect(response.body).to include(user.email)
   end
 
 end
