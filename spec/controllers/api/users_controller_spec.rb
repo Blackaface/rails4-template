844f05a36ccf388d319997341942efee12224113
marsz <marsz330@gmail.com>
2016-12-01 15:44:59 +0800

    controller rspec basic usage

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
 
+  let!(:user) { User.create!(email: 'xxx@example.com', password: '12341234') }
+
+  before do
+    # respond_error unless request.headers.to_h["HTTP_X_TOKEN"] == "123456"
+    request.headers['HTTP_X_TOKEN'] = '123456'
+  end
+
+  it '#index' do
+
+    get :index, abc: 1
+    expect(response.status).to eq(200)
+  end
 end
